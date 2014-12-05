84af10786d44e1fe83dd6947e3ac36a9c953bc38
ChrjapinAN <ChrjapinAN@a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d>
2014-12-05 02:07:40 +0000

    git-svn-id: svn:///U:%5CRepos%5CASUTP.Library@29 a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d

---
+++ trunk/HInterfacePlugIn.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+
+namespace HClassLibrary
+{
+    public class EventArgsDataHost {
+        public int id;
+        public object par;
+
+        public EventArgsDataHost (int id_, object o) {
+            id = id_;
+            par = o;
+        }
+    }
+    
+    public interface IPlugIn
+    {
+        IPlugInHost Host { get; set; }
+
+        object Object { get; }
+    }
+
+    public abstract class HPlugIn : IPlugIn
+    {
+        IPlugInHost _host;
+        protected object _object;
+        public Int16 _Id;
+
+        public IPlugInHost Host
+        {
+            get { return _host; }
+            set
+            {
+                _host = value;
+                _host.Register(this);
+            }
+        }
+
+        public HPlugIn () : base () {
+            //EvtDataRecievedHost += new DelegateObjectFunc(OnEvtDataRecievedHost);
+        }
+
+        protected abstract bool createObject();
+
+        public object Object
+        {
+            get
+            {
+                createObject();
+
+                return _object;
+            }
+        }
+
+        protected void DataAskedHost(object par)
+        {
+            //Вариант №1 - без потока
+            EvtDataAskedHost.BeginInvoke(new EventArgsDataHost (_Id, par), new AsyncCallback(this.dataRecievedHost), new Random ());
+
+            ////Вариант №2 - спотоком
+            //Thread thread = new Thread (new ParameterizedThreadStart (dataAskedHost));
+            //thread.Start(par);
+        }
+
+        ////Потоковая функция для вар.№2
+        //private void dataAskedHost (object obj) {
+        //    IAsyncResult res = EvtDataAskedHost.BeginInvoke(obj, new AsyncCallback(this.dataRecievedHost), null);
+        //}
+
+        private void dataRecievedHost(object res)
+        {
+            if ((res as AsyncResult).EndInvokeCalled == false)
+                ; //((DelegateObjectFunc)((AsyncResult)res).AsyncDelegate).EndInvoke(res as AsyncResult);
+            else
+                ;
+        }
+
+        //Int16 IdOwnerMenuItem { get; }
+        public abstract string NameOwnerMenuItem { get; }
+
+        public abstract string NameMenuItem { get; }
+
+        public abstract void OnClickMenuItem (object obj, EventArgs ev);
+
+        public event DelegateObjectFunc EvtDataAskedHost;
+        //public event DelegateObjectFunc EvtDataRecievedHost;
+        public /*protected*/ abstract void OnEvtDataRecievedHost(object obj);
+    }
+
+    public interface IPlugInHost
+    {
+        bool Register(IPlugIn plug);
+    }
+}
--- trunk/HTabCtrlEx.Designer.cs
+++ trunk/HTabCtrlEx.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace HClassLibrary
+﻿using System.ComponentModel;
+
+namespace HClassLibrary
 {
     partial class HTabCtrlEx
     {
@@ -40,5 +42,51 @@
         }
 
         #endregion
+
+        private class HTabPageEx : System.Windows.Forms.TabPage
+        {
+            public HTabPageEx()
+            {
+                InitializeComponent();
+            }
+
+            public HTabPageEx(IContainer container)
+            {
+                container.Add(this);
+
+                InitializeComponent();
+            }
+
+            /// <summary>
+            /// Требуется переменная конструктора.
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
+
+            /// <summary> 
+            /// Освободить все используемые ресурсы.
+            /// </summary>
+            /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            #region Код, автоматически созданный конструктором компонентов
+
+            /// <summary>
+            /// Обязательный метод для поддержки конструктора - не изменяйте
+            /// содержимое данного метода при помощи редактора кода.
+            /// </summary>
+            private void InitializeComponent()
+            {
+                components = new System.ComponentModel.Container();
+            }
+
+            #endregion
+        }
     }
 }
