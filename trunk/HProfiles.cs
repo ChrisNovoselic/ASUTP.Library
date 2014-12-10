63cf3ae230e9830a13aeab48cfb28750cb4c4fb0
ChrjapinAN <ChrjapinAN@a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d>
2014-12-10 10:52:41 +0000

    git-svn-id: svn:///U:%5CRepos%5CASUTP.Library@32 a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d

---
+++ trunk/HProfiles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HClassLibrary
+{
+    public class HProfiles
+    {
+        protected static int [] s_arItemIDs;
+        
+        public HProfiles(int iListenerId, int id_role, int id_user)
+        {
+        }
+
+        protected void initializeItemIDs () {
+        }
+    }
+}
