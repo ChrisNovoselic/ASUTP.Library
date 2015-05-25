696395a6c58b996523853eae1cd22cec60aa17bd
Devil <Devil@a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d>
2015-05-25 17:40:06 +0000

    git-svn-id: svn:///U:%5CRepos%5CASUTP.Library@113 a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d

--- trunk/HHandlerDb.cs
+++ trunk/HHandlerDb.cs
@@ -58,9 +58,9 @@ namespace HClassLibrary
         /// <param name="indx">Тип источника информации</param>
         /// <param name="connSett">Параметры соединения с источником информации</param>
         /// <param name="name">Наименование источника информации</param>
-        protected virtual void register(int id, int indx, ConnectionSettings connSett, string nameTEC)
+        protected virtual void register(int id, int indx, ConnectionSettings connSett, string name)
         {
-            m_dictIdListeners[id][indx] = DbSources.Sources().Register(connSett, true, @"ТЭЦ=" + nameTEC + @", DESC=" + indx.ToString());
+            m_dictIdListeners[id][indx] = DbSources.Sources().Register(connSett, true, @"ИстчнкИнфо=" + name + @", DESC=" + indx.ToString());
         }
         /// <summary>
         /// Старт обработки запросов
