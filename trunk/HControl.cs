9e019e7aee51e9f88861f1640f16dc4ad584dc52
ChrjapinAN <ChrjapinAN@a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d>
2015-12-21 03:50:53 +0000

    git-svn-id: svn:///U:%5CRepos%5CASUTP.Library@210 a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d

--- trunk/HControl.cs
+++ trunk/HControl.cs
@@ -776,4 +776,43 @@ namespace HClassLibrary
             this.CellTemplate = new DataGridViewDisableButtonCell();
         }
     }
+
+    public class DropDownButton : Button
+    {
+        int rightPadding = 16;
+
+        public DropDownButton()
+        {
+            Padding = new Padding(0, 0, rightPadding, 0);
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+
+            var rect = new Rectangle(ClientRectangle.Width - rightPadding, ClientRectangle.Top + 3, rightPadding - 3, ClientRectangle.Height - 6);
+
+            int x = ClientRectangle.Width - rightPadding + 3;
+            int y = ClientRectangle.Height / 2 - 1;
+
+            var brush = Enabled ? SystemBrushes.ControlText : SystemBrushes.ButtonShadow;
+            var arrows = new Point[] { new Point(x, y), new Point(x + 7, y), new Point(x + 3, y + 4) };
+            pevent.Graphics.FillPolygon(brush, arrows);
+            pevent.Graphics.DrawLine(Pens.Silver, rect.Left, rect.Top, rect.Left, rect.Bottom);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (e.X > Width - rightPadding && e.Button == MouseButtons.Left)
+            {
+                if (ContextMenuStrip != null)
+                {
+                    ContextMenuStrip.MinimumSize = new Size(Width - rightPadding, 0);
+                    ContextMenuStrip.Show(this, 0, Height);
+                }
+            }
+            else
+                base.OnMouseDown(e);
+        }
+    }
 }
