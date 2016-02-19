45a45bfd608fe33c87df903bd132c4764012aa1d
ChrjapinAN <ChrjapinAN@a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d>
2016-02-19 02:42:50 +0000

    git-svn-id: svn:///U:%5CRepos%5CASUTP.Library@240 a0a3717d-cbde-dc42-a72d-e28d7a1a1f6d

---
+++ branches/users_001/FormParametersBase.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace HClassLibrary
+{
+    public abstract partial class FormParametersBase : Form
+    {
+        public System.Windows.Forms.Button btnOk;
+        public System.Windows.Forms.Button btnReset;
+        public System.Windows.Forms.Button btnCancel;
+
+        public bool mayClose;
+        //private DelegateFunc delegateParamsApply;
+
+        public Int16 m_State;
+
+        public FormParametersBase () {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            //this.btnOk.Location = new System.Drawing.Point(61, 320);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 39;
+            this.btnOk.Text = "Применить";
+            this.btnOk.UseVisualStyleBackColor = true;
+            //this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnReset
+            // 
+            //this.btnReset.Location = new System.Drawing.Point(154, 320);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 40;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            //this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCancel
+            // 
+            //this.btnCancel.Location = new System.Drawing.Point(247, 320);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 41;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnOk);
+
+            this.KeyUp +=new KeyEventHandler(FormParametersBase_KeyUp);
+
+            m_State = 0;
+        }
+
+        public abstract void Update (out int err);
+
+        protected abstract void loadParam(bool bInit);
+
+        protected abstract void saveParam();
+        
+        protected void Parameters_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!mayClose)
+                e.Cancel = true;
+            else
+                mayClose = false;
+        }
+
+        //protected abstract void btnOk_Click(object sender, EventArgs e);
+
+        protected virtual void buttonCancel_Click(object sender, EventArgs e)
+        {
+            mayClose = true;
+            Close();
+        }
+
+        private void FormParametersBase_KeyUp(object obj, KeyEventArgs ev)
+        {
+            if (ev.KeyCode == Keys.Escape) {
+                btnCancel.PerformClick ();
+            }
+            else
+                ;
+        }
+    }
+}
---
+++ branches/users_001/HTabCtrlEx.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Reflection;
+
+namespace HClassLibrary
+{
+    public partial class HTabCtrlEx : System.Windows.Forms.TabControl
+    {
+        public enum TYPE_TAB { FIXED, FLOAT };
+        protected struct PropertyTab {
+            public PropertyTab(int id, TYPE_TAB type) { this.id = id; this.type = type; }
+
+            public int id;
+            public TYPE_TAB type;           
+        }       
+
+        protected  List<PropertyTab> m_listPropTabs;
+
+        private static RectangleF s_rectPositionImg = new RectangleF (18, 4, 14, 14);
+
+        private enum INDEX_BITMAP {FLOAT, CLOSE, COUNT_BITMAP };
+        private enum INDEX_STATE_BITMAP { NON_ACTIVE, IN_ACTIVE, COUNT_STATE_BITMAP };
+        private Icon [] m_arBitmap;
+        public delegate void DelegateHTabCtrlEx(object sender, HTabCtrlExEventArgs e);
+        public event DelegateHTabCtrlEx EventHTabCtrlExClose;
+        public event DelegateHTabCtrlEx EventHTabCtrlExFloat;
+
+        private int iPrevSelectedIndex;
+        public int PrevSelectedIndex
+        {
+            get { return iPrevSelectedIndex; }
+            set { if (!(iPrevSelectedIndex == value)) { EventPrevSelectedIndexChanged(iPrevSelectedIndex); iPrevSelectedIndex = value; } else ; }
+        }
+        public event DelegateIntFunc EventPrevSelectedIndexChanged;
+
+        public HTabCtrlEx()
+        {
+            InitializeComponent();
+
+            iPrevSelectedIndex = -1;
+        }
+
+        public HTabCtrlEx(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+
+            iPrevSelectedIndex = -1;
+        }
+
+        private bool confirmOnClose = true;
+        public bool ConfirmOnClose
+        {
+            get
+            {
+                return this.confirmOnClose;
+            }
+            set
+            {
+                this.confirmOnClose = value;
+            }
+        }
+
+        private Bitmap getBitmap (INDEX_BITMAP indx, INDEX_STATE_BITMAP state)
+        {
+            return m_arBitmap[(int)indx * (int)INDEX_STATE_BITMAP.COUNT_STATE_BITMAP + (int)state].ToBitmap ();
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// override to draw the close button
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="e"></param></span>
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            RectangleF tabTextAreaText = RectangleF.Empty
+                , tabTextAreaImg = RectangleF.Empty;
+            for (int nIndex = 0; nIndex < this.TabCount; nIndex++)
+            {
+                Image img;
+                INDEX_STATE_BITMAP state;
+                tabTextAreaText = (RectangleF)this.GetTabRect(nIndex);
+                //e.Graphics.IntersectClip(tabTextAreaText);
+                tabTextAreaImg = new RectangleF(tabTextAreaText.X + tabTextAreaText.Width - s_rectPositionImg.X, s_rectPositionImg.Y, s_rectPositionImg.Width, s_rectPositionImg.Height);
+                //if (nIndex > 0) {
+                    if (! (nIndex == this.SelectedIndex))
+                    {
+                        state = INDEX_STATE_BITMAP.NON_ACTIVE;
+                    }
+                    else
+                    {
+                        state = INDEX_STATE_BITMAP.IN_ACTIVE;
+                    }
+
+                    using (img = getBitmap(INDEX_BITMAP.CLOSE, state))
+                    {
+                        e.Graphics.DrawImage(img, tabTextAreaImg);
+                        //Console.WriteLine (@"OnDrawItem () - " + @"Индекс=" + nIndex + @"; X:" + tabTextArea.X + @"; width:" + tabTextArea.Width);
+                    }
+
+                    if (m_listPropTabs[nIndex].type == TYPE_TAB.FLOAT) {
+                        tabTextAreaImg = new RectangleF(tabTextAreaImg.X - (s_rectPositionImg.Width + 1), s_rectPositionImg.Y, s_rectPositionImg.Width, s_rectPositionImg.Height);
+
+                        using (img = getBitmap(INDEX_BITMAP.FLOAT, state))
+                        {
+                            e.Graphics.DrawImage(img, tabTextAreaImg);
+                        }
+                    }
+                    else
+                        ;
+                //}
+                //else
+                //    ;
+
+                string str = this.TabPages[nIndex].Text;
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Near;
+                using (SolidBrush brush = new SolidBrush(this.TabPages[nIndex].ForeColor))
+                {
+                    /*Draw the tab header text*/
+                    e.Graphics.DrawString(str, this.Font, brush, tabTextAreaText, stringFormat);
+                }
+            }
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Get the stream of the embedded bitmap image
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="filename"></param></span>
+        /// <span class="code-SummaryComment"><returns></returns></span>
+        private Stream GetContentFromResource(string filename)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly ();
+            Stream stream = asm.GetManifestResourceStream ("HTabCtrlLibrary." + filename);
+
+            return stream;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (DesignMode == false)
+            {
+                Image img;
+                INDEX_STATE_BITMAP state;
+                Graphics g = CreateGraphics();
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                for (int nIndex = 0; nIndex < this.TabCount; nIndex++)
+                {
+                    RectangleF tabTextAreaText = (RectangleF)this.GetTabRect(nIndex)
+                        , tabTextAreaClose = new RectangleF(tabTextAreaText.X + tabTextAreaText.Width - s_rectPositionImg.X, s_rectPositionImg.Y, s_rectPositionImg.Width, s_rectPositionImg.Height);
+                    //Console.WriteLine(@"OnMouseMove () - " + @"Индекс=" + nIndex + @"; X:" + tabTextArea.X + @"; width:" + tabTextArea.Width);
+
+                    Point pt = new Point(e.X, e.Y);
+                    if (tabTextAreaClose.Contains(pt) == true)
+                    {
+                        state = INDEX_STATE_BITMAP.IN_ACTIVE;
+                    }
+                    else
+                    {
+                        if (!(nIndex == this.SelectedIndex))
+                        {
+                            state = INDEX_STATE_BITMAP.NON_ACTIVE;
+                        }
+                        else
+                        {
+                            state = INDEX_STATE_BITMAP.IN_ACTIVE;
+                        }
+                    }
+
+                    img = getBitmap(INDEX_BITMAP.CLOSE, state);
+
+                    using (img)
+                    {
+                        g.DrawImage(img, tabTextAreaClose);
+                    }
+
+                    if (m_listPropTabs[nIndex].type == TYPE_TAB.FLOAT)
+                    {
+                        RectangleF tabTextAreaFloat = new RectangleF(tabTextAreaClose.X - (s_rectPositionImg.Width + 1), s_rectPositionImg.Y, s_rectPositionImg.Width, s_rectPositionImg.Height);
+                        
+                        if (tabTextAreaFloat.Contains(pt))
+                        {
+                            state = INDEX_STATE_BITMAP.IN_ACTIVE;
+                        }
+                        else
+                        {
+                            if (!(nIndex == this.SelectedIndex))
+                            {
+                                state = INDEX_STATE_BITMAP.NON_ACTIVE;
+                            }
+                            else
+                            {
+                                state = INDEX_STATE_BITMAP.IN_ACTIVE;
+                            }
+                        }
+
+                        img = getBitmap(INDEX_BITMAP.FLOAT, state);
+
+                        g.DrawImage(img, tabTextAreaFloat);
+                    }
+                    else
+                        ;
+                }
+                g.Dispose();
+            }
+            else
+                ;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if ((DesignMode == false)
+                /* && (SelectedIndex > 0)*///Здесь запрет закрыть вкладку с индексом "0"
+                //&& (TabPages.Count > 1)
+                )
+            {
+                RectangleF tabTextAreaText = (RectangleF)this.GetTabRect(SelectedIndex)
+                    , tabTextAreaClose = new RectangleF(tabTextAreaText.X + tabTextAreaText.Width - s_rectPositionImg.X, s_rectPositionImg.Y, s_rectPositionImg.Width, s_rectPositionImg.Height);
+                Point pt = new Point(e.X, e.Y);
+                if (tabTextAreaClose.Contains(pt) == true)
+                {
+                    if (confirmOnClose == true)
+                    {
+                        if (MessageBox.Show("Вы закрывайте вкладку с объектом: " + this.TabPages[SelectedIndex].Text.TrimEnd() + ". Продолжить?", "Подтверждение", MessageBoxButtons.YesNo) == DialogResult.No)
+                            return;
+                        else
+                            ;
+                    }
+                    else
+                        ;
+
+                    //Fire Event to Client
+                    if (! (EventHTabCtrlExClose == null))
+                    {
+                        EventHTabCtrlExClose(this, new HTabCtrlExEventArgs(m_listPropTabs[SelectedIndex].id, SelectedIndex, this.TabPages[SelectedIndex].Text.Trim()));
+                    }
+                    else
+                        ;
+                }
+                else {
+                    if (m_listPropTabs[SelectedIndex].type == TYPE_TAB.FLOAT)
+                    {
+                        RectangleF tabTextAreaFloat = new RectangleF(tabTextAreaClose.X - (s_rectPositionImg.Width + 1), s_rectPositionImg.Y, s_rectPositionImg.Width, s_rectPositionImg.Height);
+                        
+                        if (tabTextAreaFloat.Contains(pt) == true)
+                        {
+                            if (!(EventHTabCtrlExFloat == null))
+                            {
+                                EventHTabCtrlExFloat(this, new HTabCtrlExEventArgs(m_listPropTabs[SelectedIndex].id, SelectedIndex, this.TabPages[SelectedIndex].Text.Trim()));
+                            }
+                            else
+                                ;
+                        } else {
+                        }
+                    } else {
+                    }
+
+                }
+            }
+        }
+
+        //public void TabPagesClear()
+        //{
+        //    while (TabCount > 1)
+        //        TabPages.RemoveAt (TabCount - 1);
+
+        //    m_listTypeTabs.Clear ();
+        //}
+
+        public void AddTabPage (string name, int id, TYPE_TAB typeTab) {
+            m_listPropTabs.Add(new PropertyTab (id, typeTab));
+            this.TabPages.Add(name, getNameTab(name, typeTab));
+        }
+
+        public bool RemoveTabPage (string name) {
+            bool bRes = RemoveTabPage (this.TabPages.IndexOfKey (name.Trim ()));
+
+            if (bRes == false)
+                Console.WriteLine (@"Ошибка удаления вкладки [" + name + "]...");
+            else
+                ;
+
+            return bRes;
+        }
+
+        public bool RemoveTabPage(int indx)
+        {
+            bool bRes = true;
+            
+            if ((! (indx < 0))
+                && (indx < this.TabCount)
+                && (indx < m_listPropTabs.Count))
+            {
+                m_listPropTabs.RemoveAt(indx);
+                this.TabPages.RemoveAt(indx);              
+            }
+            else
+                bRes = false;
+
+            return bRes;
+        }
+
+        //public string NameOfItemControl (Control ctrl)
+        //{
+        //    return TabPages[IndexOfItemControl (ctrl)].Text;
+        //}
+
+        //public int IndexOfItemControl (Control ctrl) {
+        //    int iRes = -1
+        //        , indx = 0;
+
+        //    while ((indx < TabCount) && (iRes < 0)) {
+        //        //if (TabPages [indx].Controls.Contains (ctrl) == true)                
+        //        if (TabPages[indx].Controls[0].Equals (ctrl) == true) //??? тоже не работает
+        //            iRes = indx;
+        //        else
+        //            ;
+
+        //        indx ++;
+        //    }
+
+        //    return iRes;
+        //}
+
+        private static string getNameTab (string text, TYPE_TAB type) {
+            int cntSpace = 5;
+
+            if (type == TYPE_TAB.FLOAT)
+                cntSpace += 4;
+            else
+                ;
+
+            return new string(' ', 1) + text + new string(' ', cntSpace);
+        }
+
+        public int IndexOfID(int id)
+        {
+            int iRes = -1;
+
+            foreach (PropertyTab propTab in m_listPropTabs)
+                if (propTab.id == id)
+                {
+                    iRes = m_listPropTabs.IndexOf(propTab);
+
+                    break;
+                }
+                else
+                    ;
+
+            return iRes;
+        }
+
+        public string VisibleIDs
+        {
+            get
+            {
+                string strRes = string.Empty;
+
+                foreach (PropertyTab tab in m_listPropTabs)
+                    strRes += tab.id + @",";
+
+                if (strRes.Length > 1)
+                    strRes = strRes.Substring(0, strRes.Length - 1);
+                else
+                    ;
+
+                return strRes;
+            }
+        }
+    }
+
+    public class HTabCtrlExEventArgs : EventArgs
+    {
+        private int nTabIndex = -1;
+        private string strHeaderText = string.Empty;
+        private HTabCtrlEx.TYPE_TAB typeTab;
+        private int nId;
+
+        public HTabCtrlExEventArgs(int nId, int nTabIndex, string text)
+        {
+            this.nTabIndex = nTabIndex;
+            this.strHeaderText = text;
+            this.nId = nId;
+        }
+        /// <summary>
+        /// Get/Set the tab index value where the close button is clicked
+        /// </summary>
+        public int TabIndex { get { return this.nTabIndex; } set { this.nTabIndex = value; } }
+
+        /// <summary>
+        /// Get/Set the tab index value where the close button is clicked
+        /// </summary>
+        public string TabHeaderText { get { return this.strHeaderText; } set { this.strHeaderText = value; } }
+
+        public HTabCtrlEx.TYPE_TAB TabType { get { return this.typeTab; } set { this.typeTab = value; } }
+
+        public int Id { get { return this.nId; } set { this.nId = value; } }
+    }
+};
